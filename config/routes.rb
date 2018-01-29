cfc1822375de4dd9cc8180684fb2bd4cb0055dbf
Brad Carlson <b2carlso@yahoo.com>
2018-01-29 00:49:03 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 RailsBlog::Application.routes.draw do
 
-<<<<<<< HEAD
+
   resources :post_tags
 
-=======
->>>>>>> 255b09e86441e4d6bfa8a7ca62cb6d9233f13caa
+
   resources :tags
 
   resources :users
